5db8c56c928b30d3de155fb92e793fc9251c8e6b
Esmée Zootjes <esmeezootjes@gmail.com>
2023-11-23 11:36:54 +0100

    filter is functional on map list

--- routes/add-to-list.js
+++ routes/add-to-list.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
-const FavoritePlace = require('../models/FavoritePlace.model');
+const FavoritePlace = require("../models/FavoritePlace.model");
 const path = require("path");
 const fs = require("fs");
+const Place = require("../models/Place.model");
 
 router.post("/add-to-list", (req, res) => {
   const { name } = req.body;
-  console.log(req.body.name);
+  // console.log(req.body.name);
 
   User.findByIdAndUpdate(
     req.session.currentUser._id,
@@ -45,13 +46,104 @@ router.post("/remove-from-list", (req, res) => {
     });
 });
 
-router.get("/create-list", (req, res) => {
-  const placesFilePath = path.join(__dirname, "../db/data-places.json");
+// router.post("/select-category", (req, res) => {
+//   const { category } = req.body;
+//   // console.log(category);
 
+//   Place.findOneAndUpdate(
+//     { category: category },
+//     { $push: { selectedCategory: category } },
+//     { new: true }
+//   )
+//     .then((place) => {
+//       // console.log(place.selectedCategory);
+//       res.redirect("create-list");
+//       // res.render("create-list", { favoriteList: user.favoriteList });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// // werkt nog niet, uitprobeer route
+// router.get("/create-list", (req, res) => {
+//   const selectedCategory = req.query.category;
+//   console.log(selectedCategory);
+//   Place.find({ category: selectedCategory })
+//     .then((places) => {
+//       User.findById(req.session.currentUser._id)
+//         .then((user) => {
+//           const favorites = user.favoriteList;
+
+//           const filteredPlaces = places.filter(
+//             (place) => !favorites.includes(place.name)
+//           );
+
+//           let categoryFilteredPlaces = filteredPlaces;
+
+//           if (selectedCategory && selectedCategory !== "all") {
+//             categoryFilteredPlaces = filteredPlaces.filter(
+//               (place) => place.selectedCategory === selectedCategory
+//             );
+//           }
+
+//           res.render("create-list", {
+//             places: categoryFilteredPlaces,
+//             favorites: favorites,
+//             username: user.username,
+//           });
+//         })
+//         .catch((userErr) => {
+//           console.error("User findById Error:", userErr);
+//           res.status(500).send("Internal Server Error");
+//         });
+//     })
+//     .catch((placeErr) => {
+//       console.error("Place find Error:", placeErr);
+//       res.status(500).send("Internal Server Error");
+//     });
+// }); // werkt nog niet!
+
+router.get("/create-list", async (req, res) => {
+  const { category } = req.query;
+  const placesFilePath = path.join(__dirname, "../db/data-places.json");
+  const foundUser = await User.findById(req.session.currentUser._id);
+  let allCategories = await Place.find();
+  allCategories = allCategories.map((place) => place.category);
+  allCategories.unshift("all");
+  allCategories = [...new Set(allCategories)];
+  allCategories = allCategories.filter((place) => place !== category);
+  fs.readFile(placesFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading places JSON file:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const places = JSON.parse(data); // Parse JSON data
+    let filteredPlaces = places.filter(
+      (place) => !foundUser.favoriteList.includes(place.name)
+    );
+
+    if (category) {
+      filteredPlaces = places.filter((place) => place.category === category);
+    }
+
+    // return res.send(filteredPlaces);
+
+    res.render("create-list", {
+      places: filteredPlaces,
+      favorites: foundUser.favoriteList,
+      username: foundUser.username,
+      allCategories,
+      category: category === "all" ? null : category,
+    }); // Pass places data to the template
+    // res.send(places);
+  });
+  return;
   User.findById(req.session.currentUser._id)
     .then((user) => {
       let favorites = user.favoriteList;
-      console.log(favorites);
+      // console.log(favorites);
 
       fs.readFile(placesFilePath, "utf8", (err, data) => {
         if (err) {
@@ -69,6 +161,7 @@ router.get("/create-list", (req, res) => {
           places: filteredPlaces,
           favorites: favorites,
           username: user.username,
+          category: category,
         }); // Pass places data to the template
         // res.send(places);
       });
